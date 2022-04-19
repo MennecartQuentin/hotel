8428ae840ff704b6b4afb97a64ac78f1bfbf0907
MennecartQuentin <mennecartquentin16@gmail.com>
2022-04-19 15:03:46 +0200

    modif connection

--- connection.php
+++ connection.php
@@ -1,3 +1,20 @@
 <?php 
-$con=mysqli_connect("localhost","root","","hotel") or die('La connexion à la base de données a échoué');
+// Dev connection
+//$host = '127.0.0.1';
+//$db = 'hotel';
+//$user = 'root';
+//$pass = '';
+//$charset = 'utf8mb4';
+
+
+//Remote Database Connection
+$host = 'remotemysql.com';
+$db = '0Ly3QV748r';
+$user = '0Ly3QV748r';
+$pass = 'uzHo5IjHkU';
+$charset = 'utf8mb4';
+
+$con=mysqli_connect("host=$host;dbname=$db;charset=$charset")or die('La connexion à la base de données a échoué');
+
+//$con=mysqli_connect("localhost","root","","hotel") or die('La connexion à la base de données a échoué');
 ?>
