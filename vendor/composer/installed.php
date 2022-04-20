d6c0df1af02955b2ae8f0b9772598ed6fc7605e1
MennecartQuentin <mennecartquentin16@gmail.com>
2022-04-20 17:31:40 +0200

    ajout package

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3f5cc6613bffc7984740b0c4cfa7676994a3e61a',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3f5cc6613bffc7984740b0c4cfa7676994a3e61a',
+            'dev_requirement' => true,
+        ),
+    ),
+);
