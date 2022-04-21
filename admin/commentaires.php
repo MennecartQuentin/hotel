24308c9cf73ba1bf71f2f67d28bf68dab2fcbdcb
MennecartQuentin <mennecartquentin16@gmail.com>
2022-04-21 16:07:31 +0200

    ajout de th dans commentaire

--- admin/commentaires.php
+++ admin/commentaires.php
@@ -3,17 +3,18 @@
 	{
 		if(confirm("You want to delete this Feedback ?"))
 		{
-		window.location.href='supprimer commentaires.php?id='+id;	
+		window.location.href='supprimer message.php?id='+id;	
 		}
 	}
 </script>
 <table class="table table-striped table-hover">
-	<h1>Commentaires</h1><hr>
+	<h1>Contact</h1><hr>
 	<tr>
-		<th>Numéro</th>
+	<th>Numéro</th>
 		<th>Nom</th>
+		<th>Prénom</th>
 		<th>Email</th>
-		<th>Téléphone</th>
+		<th>Option</th>
 		<th>Message</th>
 		<th>Supprimer</th>
 	</tr>
@@ -22,17 +23,19 @@ $i=1;
 $sql=mysqli_query($con,"select * from feedback");
 while($res=mysqli_fetch_assoc($sql))
 {
-$id=$res['id'];	
-$name=$res['name'];
-$email=$res['email'];
-$mobile	=$res['mobile'];
-$message=$res['message'];
+	$id=$res['id'];	
+	$name=$res['name'];
+	$lastname=$res['prenom'];
+	$email=$res['email'];
+	$mobile	=$res['option'];
+	$message=$res['message'];
 ?>
 <tr>
-		<td><?php echo $i;$i++; ?></td>
+<td><?php echo $i;$i++; ?></td>
 		<td><?php echo $res['name']; ?></td>
+		<td><?php echo $res['prenom']; ?></td>
 		<td><?php echo $res['email']; ?></td>
-		<td><?php echo $res['mobile']; ?></td>
+		<td><?php echo $res['option']; ?></td>
 		<td><?php echo $res['message']; ?></td>
 		<td><a href="#"onclick="delFeedback('<?php echo $id; ?>')"><span class="glyphicon glyphicon-remove"style='color:red'></span></a></td>
 	</tr>	
